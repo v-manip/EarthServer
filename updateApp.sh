396202604c94adb7abb95988c7b704cc94c9fda6
Martin Hecher <martin.hecher@fraunhofer.at>
2014-01-24 17:15:41 +0100

    Added helper script for app development

---
+++ updateApp.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+rsync -avp ~/Projekte/vmanip/RectangularBoxViewer/dist/EarthServerClient.js ~/Projekte/vmanip/WebClient-Framework/app/bower_components/rectangularboxviewer/js/EarthServerClient.js
