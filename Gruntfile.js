ae3a2692771eb25d0618550047ea23dfbc4df3c1
Martin Hecher <martin.hecher@fraunhofer.at>
2014-01-29 10:39:15 +0100

    Grunt: added copy task to simplify development

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,28 @@
 module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 
 	grunt.initConfig({
+		// Installation directory based on the V-MANIP organization repository:
+		installdir: '../WebClient-Framework/app/bower_components/rectangularboxviewer/',
 		pkg: grunt.file.readJSON('package.json'),
 		concat: {
 			options: {
 				separator: '\n'
 			},
 			dist: {
-				src: ['js/SceneManager.js', 'js/*.js', '!js/EarthServerClient*.js', '!js/UI_v1.js'],
-				dest: 'dist/<%= pkg.name %>.js'
+				// FIXXME: Module_WMSDemWMS.js has to be manually included, for some reason...
+				src: ['js/SceneManager.js', 'js/*.js', 'js/Module_WMSDemWMS.js', '!js/EarthServerClient*.js', '!js/UI_v1.js'],
+				dest: 'js/<%= pkg.name %>_DailyBuild.js'
 			}
-		}
+		},
+		copy: {
+			main: {
+				src: '<%= concat.dist.dest %>',
+				dest: '<%= installdir %>'
+			},
+		},
 	});
 
-	grunt.registerTask('default', ['concat']);
+	grunt.registerTask('default', ['concat', 'copy']);
 };
