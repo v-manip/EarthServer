2fdcd084f024e9a42fa865f540183f315c0dde28
Martin Hecher <martin.hecher@fraunhofer.at>
2014-02-07 14:17:33 +0100

    Removed old dev script

--- updateApp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-rsync -avp ~/Projekte/vmanip/RectangularBoxViewer/dist/EarthServerClient.js ~/Projekte/vmanip/WebClient-Framework/app/bower_components/rectangularboxviewer/js/EarthServerClient.js
-rsync -avp ~/Projekte/vmanip/RectangularBoxViewer/js/Module*.js ~/Projekte/vmanip/WebClient-Framework/app/bower_components/rectangularboxviewer/js/
